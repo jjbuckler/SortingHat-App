99144f39eddaf6af1f70565c10c298fed13237aa
jjbuckler <jjbuckler@gmail.com>
2018-10-05 13:07:54 -0400

    format
    formatted code

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,29 @@
-
-
 const y = document.getElementById("sortingHat");
-const houseLabel =  document.getElementById("houseName")
+const houseLabel = document.getElementById("houseName")
 // Code for Chrome, Safari and Opera
 y.addEventListener("webkitAnimationEnd", myEndFunction);
 
-function startSort(){
+function startSort() {
   console.log("here")
- houseLabel.className="houseInvisible"; 
-  y.className="sorting";
+  houseLabel.className = "houseInvisible";
+  y.className = "sorting";
 }
-function myEndFunction(){
-  var x=Math.floor(Math.random() * 13);
-  if(x<=2){
-    houseLabel.innerHTML="Gryffindor!";
 
-  } else if(x>2 && x<=5){
+function myEndFunction() {
+  var x = Math.floor(Math.random() * 13);
+  if (x <= 2) {
+    houseLabel.innerHTML = "Gryffindor!";
 
-    houseLabel.innerHTML="Slytherin!";
+  } else if (x > 2 && x <= 5) {
 
+    houseLabel.innerHTML = "Slytherin!";
+
+  } else if (x > 5 && x <= 9) {
+    houseLabel.innerHTML = "Hufflepuff!";
+  } else {
+    houseLabel.innerHTML = "Ravenclaw!"
   }
-  else if (x>5 && x<=9){
-    houseLabel.innerHTML="Hufflepuff!";
-  } else{
-    houseLabel.innerHTML="Ravenclaw!"
-  }
-  y.className="";
-  houseLabel.className="houseVisible";
+  y.className = "";
+  houseLabel.className = "houseVisible";
 }
 /*Modernizr */
